4721db99915ccb3479466ec2c722869dad30cef0
thelazylama <67310144+thelazylamaGit@users.noreply.github.com>
2026-01-15 01:48:20 +1100

    Create ScreenQuad.svelte
    Adds port of drei ScreenQuad shape component

---
+++ packages/extras/src/lib/components/ScreenQuad/ScreenQuad.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import { T } from '@threlte/core'
+  import { BufferGeometry, BufferAttribute, Sphere, Vector3 } from 'three/webgpu'
+
+  const fullscreenTriGeometry = (() => {
+    const geometry = new BufferGeometry()
+
+    const positions = new Float32Array([-1.0, -1.0, 3.0, -1.0, -1.0, 3.0])
+
+    const uvs = new Float32Array([0.0, 0.0, 2.0, 0.0, 0.0, 2.0])
+
+    geometry.setAttribute('position', new BufferAttribute(positions, 2))
+    geometry.setAttribute('uv', new BufferAttribute(uvs, 2))
+
+    //Prevents Error: THREE.BufferGeometry.computeBoundingSphere(): Computed radius is NaN.
+    geometry.boundingSphere = new Sphere(new Vector3(), Infinity)
+
+    return geometry
+  })()
+
+  let { children, ...meshProps } = $props()
+</script>
+
+<T.Mesh geometry={fullscreenTriGeometry} frustumCulled={false} {...meshProps}>
+  {@render children?.()}
+</T.Mesh>
